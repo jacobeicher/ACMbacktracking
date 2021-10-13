8dd6de9ba2274282a6ee627c07da5eda161c2671
Jacob Eicher <je652917@ohio.edu>
2021-10-12 20:23:43 -0400

    full code

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,37 @@
 import numpy as np
+from numpy.lib.type_check import iscomplex
+
+def legal(puzzle, col, row, num):
+    for i in range(0,9):
+        if puzzle[col][i] == num:
+            return False
+        if puzzle[i][row] == num:
+            return False
+
+    xstart = (row//3)*3
+    ystart = (col//3)*3
+    for i in range(0,3):
+        for j in range(0,3):
+            if puzzle[ystart + i][xstart + j] == num:
+                return False
+    
+    return True
+
+
+
+def solve(puzzle):
+    for x in range(9):
+        for y in range(9):
+            if puzzle[y][x] == 0:
+                for n in range(1, 10):
+                    if legal(puzzle, y, x, n):
+                        puzzle[y][x] = n
+                        solve(puzzle)
+                        puzzle[y][x] = 0
+                return
+    print(np.matrix(puzzle))
+    return
+
 
 
 def main():
@@ -12,5 +45,6 @@ def main():
             [0, 0, 0, 4, 1, 9, 0, 0, 5], 
             [0, 0, 0, 0, 8, 0, 0, 7, 9]]
 
+    solve(puzzle)
 if __name__ == "__main__":
     main()
